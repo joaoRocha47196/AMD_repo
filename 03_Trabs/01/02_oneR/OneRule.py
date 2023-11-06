2e812c7fca6f33d0a2d6bae6b6f737f00d9ee4a7
Luís Morgado <luis.morgado@protonmail.com>
2023-11-06 16:47:10 +0000

    fix OneRule.py

--- 03_Trabs/01/02_oneR/OneRule.py
+++ 03_Trabs/01/02_oneR/OneRule.py
@@ -1,11 +1,13 @@
 from collections import defaultdict
 import Orange
+import json
+import random
 
 class OneR:
     def __init__(self, filename):
         self.dataset, self.attributes, self.target, self.domains = self.load_data(filename)
         self.freq_tables, self.total_per_atr_val = None, None
-        self.hypotheses, self.attr_accuracy, self.best_rules = None, None, None
+        self.hypotheses, self.attr_accuracy, self.best_rules, self.best_attribute = None, None, None, None
 
     def load_data(self, filename):
         data = Orange.data.Table(filename)
@@ -32,9 +34,8 @@ class OneR:
         self.freq_tables = freq_tables
         self.total_per_atr_val = total_per_atr_val
 
-
     def compute_rules_and_errors(self):
-        hypotheses, attr_accuracy,  best_rules = [], {}, {}
+        self.hypotheses, self.attr_accuracy = [], {}
 
         for atr in self.attributes:
             rules = {}
@@ -46,41 +47,83 @@ class OneR:
                 for target_val in self.domains[self.target]:
                     curr_freq = self.freq_tables[atr].get((atr_val, target_val), 0)
                     if curr_freq > max_freq:
-                        max_freq = curr_freq
-                        best_target_val = target_val
+                        max_freq, best_target_val = curr_freq, target_val
 
-                rules[atr_val] = best_target_val
-                errors[atr_val] = self.total_per_atr_val[atr][atr_val] - max_freq
-                hypotheses.append((atr, atr_val, best_target_val, errors[atr_val], self.total_per_atr_val[atr][atr_val]))
+                if best_target_val is not None:  #rule was actually found
+                    rules[atr_val] = best_target_val
+                    errors[atr_val] = self.total_per_atr_val[atr][atr_val] - max_freq
+                    self.hypotheses.append((atr, atr_val, best_target_val, errors[atr_val]))
 
-            total_error = sum(errors.values())
-            attr_accuracy[atr] = (total_error, len(self.dataset))
+            self.attr_accuracy[atr] = (sum(errors.values()), len(self.dataset))
 
-            # Determine the best attribute based on least error
-            best_attribute = min(attr_accuracy.keys(), key=lambda atr: attr_accuracy[atr][0])
-            best_rules = [(best_attribute, atr_val, rules[atr_val], errors[atr_val], self.total_per_atr_val[atr][atr_val]) for atr_val in rules.keys()]
+        # best attributes based on the lowest error rate
+        lowest_error = min(self.attr_accuracy.values(), key=lambda x: x[0])[0]
+        self.best_attributes = [atr for atr, (errors, _) in self.attr_accuracy.items() if errors == lowest_error]
 
-            self.hypotheses = hypotheses
-            self.attr_accuracy = attr_accuracy
-            self.best_rules = best_rules
+        # hypotheses with the lowest error rate
+        best_hypotheses = {}
+        for atr, atr_val, target_val, error in self.hypotheses:
+            if atr in self.best_attributes:
+                total_instances = self.total_per_atr_val[atr][atr_val]
+                if atr not in best_hypotheses:
+                    best_hypotheses[atr] = {atr_val: (target_val, error, total_instances)}
+                elif atr_val not in best_hypotheses[atr]:
+                    best_hypotheses[atr][atr_val] = (target_val, error, total_instances)
+                elif error < best_hypotheses[atr][atr_val][1]:
+                    best_hypotheses[atr][atr_val] = (target_val, error, total_instances)
 
+        # best rules from the best hypotheses
+        self.best_rules = {atr: {atr_val: (target_val, error, total_instances) for atr_val, (target_val, error, total_instances) in atr_vals.items()}
+                        for atr, atr_vals in best_hypotheses.items()}
+        print("best_rules:",self.best_rules)
+        
+    
+    def predict(self, instance):
+        print("\n- PREDICT")
+        class_votes = {target_class: 0 for target_class in self.domains[self.target]}
 
+        for atr in self.best_attributes:
+            atr_val = instance.get(atr)
+            if atr_val and atr_val in self.best_rules[atr]:
+                predicted_class = self.best_rules[atr][atr_val][0]  #  get the target_class
+                print("predicted_class:", predicted_class)
+                class_votes[predicted_class] += 1
+
+        # In case of ties or no rules applied, use a random prediction
+        most_votes = max(class_votes.values())
+        most_probable_classes = [cls for cls, votes in class_votes.items() if votes == most_votes]
+        
+        if not most_probable_classes or most_votes == 0:
+            return random.choice(list(self.domains[self.target]))
+
+        return random.choice(most_probable_classes)
 
 
     def print_hypotheses(self):
         print("- HYPOTHESES")
         for entry in self.hypotheses:
-            print(f"({entry[0]}, {entry[1]}, {entry[2]}) : ({entry[3]}, {entry[4]})")
+            print(f"({entry[0]}, {entry[1]}, {entry[2]}) : {entry[3]}")
 
     def print_attr_accuracy(self):
-        print("\n- attrACCURACY")
+        print("\n- ATTRIBUTE ACCURACY")
         for attr, acc in self.attr_accuracy.items():
-            print(f"{attr} : ({acc[0]}, {acc[1]}) # {acc[0] / acc[1]:.4f}")
+            print(f"{attr} : {acc[0]} errors out of {acc[1]} instances # Error rate: {acc[0] / acc[1]:.4f}")
 
     def print_best_rules(self):
-        print("\n- One-R")
-        for entry in self.best_rules:
-            print(f"({entry[0]}, {entry[1]}, {entry[2]}) : ({entry[3]}, {entry[4]})")
+        print("\n- BEST RULES:")
+        for atr in self.best_attributes:
+            print(f"For attribute '{atr}':")
+            for atr_val, (target_class, error, total) in self.best_rules[atr].items():
+                error_probability = f"{error}/{total}"
+                print(f"If {atr} is {atr_val}, then lenses = {target_class} # Error Probability: {error_probability}")
+
+
+    def save_model(self, file_path):
+        json_compatible_best_rules = {atr: {atr_val: list(values) for atr_val, values in atr_vals.items()}
+                                    for atr, atr_vals in self.best_rules.items()}
+        
+        with open(file_path, 'w') as f:
+            json.dump(json_compatible_best_rules, f, indent=4)
 
 
 def main():
@@ -92,6 +135,13 @@ def main():
     one_r_classifier.print_attr_accuracy()
     one_r_classifier.print_best_rules()
 
+    # Prediction:
+    new_instance = {'age': 'young', 'prescription': 'myope', 'astigmatic': 'yes', 'tear_rate': 'normal'}
+    prediction = one_r_classifier.predict(new_instance)
+    print(f"Prediction for the input values: {prediction}")
+
+    # Save Best Rules
+    one_r_classifier.save_model('one_r_model.json')
 if __name__ == "__main__":
     main()
 
