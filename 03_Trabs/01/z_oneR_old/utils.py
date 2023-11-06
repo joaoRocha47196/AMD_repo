5f80d66559d62f6014daaa758f3b332171bb3189
joaoRocha47196 <joaocrocha2001@gmail.com>
2023-11-06 20:34:54 +0000

    Updated OneR

--- 03_Trabs/01/z_oneR_old/utils.py
+++ 03_Trabs/01/z_oneR_old/utils.py
@@ -2,16 +2,6 @@ import sys
 import Orange as DM
 
 class Utils:
-   def load(self, fileName):
-      print("::[Reading Input File]")
-
-      try:
-         dataset = DM.data.Table(fileName)
-      except Exception as e:
-         self.my_print(f":: Error - cannot open the file: {fileName}")
-         sys.exit(1)  # Exit with an error code
-      return dataset
-   
    def my_print(self, aStr):
       separator = lambda x: "_" * len( x )
       print( separator( aStr ) )
@@ -25,6 +15,7 @@ class Utils:
                print(value)
       print()
 
+
    def loggHeader(self, text):
       astr = "::::::::::::::::::::::" + text + "::::::::::::::::::::::"
       separator = lambda x: ":" * len( x )
@@ -34,10 +25,42 @@ class Utils:
       print(separator(astr))
       print()
    
+   ## DELTE ALL BELOW
    def print_data_set(self, text, dataSet):
       print("::::::::::::::::::::::" + text + "::::::::::::::::::::::"  )
       print(dataSet)
       print(":::::::::::::::::::::::::::::::::::::::::::::::"  )
       print("")
 
+   def print(self, text):
+      print(text)
+
+   def log(self, text):
+      print("::" + text)
+
    
+
+   def logSimpleLine(self, ):
+      print("::::::::::::::::::::::::::::::::::::::::::::::::::::::"  )
+      print("")
+
+   def logHeader(self, text):
+      print(":: [" + text + "]")
+
+   def print_args(self, *args):
+      for arg in args:
+         print(arg)
+
+   def print_variable_and_value(self, **kwargs):
+      for name, value in kwargs.items():
+         print(f"{name}: {value}")
+
+   def load(self, fileName):
+      self.log("Reading Input File")
+
+      try:
+         dataset = DM.data.Table(fileName)
+      except Exception as e:
+         self.my_print(f":: Error - cannot open the file: {fileName}")
+         sys.exit(1)  # Exit with an error code
+      return dataset
